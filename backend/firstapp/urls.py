775d29762475a123e08682e64b73712122d737f0
70138205 <158143835+70138205@users.noreply.github.com>
2024-11-05 07:36:52 -0800

    work

--- backend/firstapp/urls.py
+++ backend/firstapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from firstapp import views
 urlpatterns=[
-    path('jan/',views.index),
-    path('feb/',views.index1),
+    # path('jan/',views.index),
+    # path('feb/',views.index1),
+    path('challenge/<str:month>/', views.index),
 
 ]
--- backend/firstapp/views.py
+++ backend/firstapp/views.py
@@ -2,9 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse('learn python')
-def index1(request):
-    return  HttpResponse('learn django')
+# def index(request,month):
+#     return HttpResponse(month)
+def index(request,year,month):   
+    if month =='jan':
+      return HttpResponse('learn django')
+    elif month == 'feb':
+      return HttpResponse('learn Django URLs')
+    else:
+       return HttpResponse('The URL you entered is not found')
+ 
+
+# def index(request):
+#     return HttpResponse('learn python')
+# def index1(request):
+#     return  HttpResponse('learn django')
     
 
